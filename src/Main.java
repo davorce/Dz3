b6ead0a3c80a3a8c802e2cb7cc1dbc3e34c08641
davorce <cepindavor@gmail.com>
2025-01-29 16:37:57 +0100

    Update 1

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //Zadatak 1. Korištenjem petlje while -> Izračunaj zbroj znamenaka unesenog broja.
+        // Npr 351 zbroj znamenaka je 9.
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Unesi broj cije znamenke zelis zbrojiti: ");
+        int uneseniBroj = scanner.nextInt();
+        int suma = 0;
+        while (uneseniBroj > 0) {
+            suma += uneseniBroj % 10;
+            uneseniBroj /= 10;
+
+        }
+
+        System.out.println("Zbroj svih znamenki unesenog broja je: " + suma+"\n\n");
+
+
+
+        //Zadatak 2. Korištenjem petlje do while izračunaj zbroj brojeva od 1 do 1000 djeljivih sa 7
+
+        int i = 1;
+        int sum = 0;
+
+        do {
+            if (i % 7 == 0){
+                sum+=i;
+            }
+            i++;
+        } while (i<=1000);
+        System.out.println("Zbroj brojeva od 1 do 1000 djeljivih sa 7 je: " + sum);
+
+    }
+}
