04a4524ecd6b2beff6aa7c2d163b21bd68b7323f
davorce <cepindavor@gmail.com>
2025-01-29 18:13:39 +0100

    Update 1

--- src/Main.java
+++ src/Main.java
@@ -11,9 +11,7 @@ public class Main {
         while (uneseniBroj > 0) {
             suma += uneseniBroj % 10;
             uneseniBroj /= 10;
-
         }
-
         System.out.println("Zbroj svih znamenki unesenog broja je: " + suma+"\n\n");
 
 
@@ -21,7 +19,6 @@ public class Main {
         //Zadatak 2. Korištenjem petlje do while izračunaj zbroj brojeva od 1 do 1000 djeljivih sa 7
         int i = 1;
         int sum = 0;
-
         do {
             if (i % 7 == 0){
                 sum+=i;
@@ -29,6 +26,5 @@ public class Main {
             i++;
         } while (i<=1000);
         System.out.println("Zbroj brojeva od 1 do 1000 djeljivih sa 7 je: " + sum);
-
     }
 }
